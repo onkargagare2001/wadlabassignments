ece0f6e590a95a1308368e7f14c8c6f5ccbffb4a
onkargagare2001 <71821005+onkargagare2001@users.noreply.github.com>
2022-05-23 11:19:26 +0530

    Add files via upload

---
+++ client.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class client{
+      private static DataOutputStream dataOutputStream =null;
+      private static DataInputStream dataInputStream =null;
+      private static Scanner scanner =new Scanner(System.in);
+      
+      
+      public static void main(String[] args){
+        try(Socket socket =new Socket("Localhost",5000)){
+        
+      dataInputStream= new DataInputStream(socket.getInputStream());
+
+dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+while (true) {
+
+System.out.print("input> " );
+
+String message =scanner.nextLine();
+
+dataOutputStream.writeUTF(message);
+ if(message.equalsIgnoreCase("exit()"))
+  break;
+ }
+
+}catch (Exception e){
+
+System.out.println(e.toString());
+      
+      }
+      
+}
+}
